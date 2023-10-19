126483ff4412349c3e9bfe9c30d3e9ae382d4c88
inmin617 <144991175+inmin617@users.noreply.github.com>
2023-10-19 15:35:39 +0900

    실습3

--- main.c
+++ main.c
@@ -5,14 +5,17 @@
 
 int main(void) 
 {
-	
 	int i;
-	for (i=0; i<5; i++) {
-		int temp=1;
-		printf("temp=%d\n", temp);
-		temp++;
-	}
-	
-	system("PAUSE");
+	i=10;
+	printf("함수 호출전 i=%d\n", i);
+	inc(i);
+	printf("함수 호출후 i=%d\n", i);
 	return 0;
+
+}
+
+int inc(int counter) 
+{
+	counter++;
+	return counter;
 }
